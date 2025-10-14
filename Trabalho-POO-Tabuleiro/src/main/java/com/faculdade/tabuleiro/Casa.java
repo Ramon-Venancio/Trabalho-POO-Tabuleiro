6fbb4fb72672b5276920019a64a1489dd27a4e94
Pedro-hmf0906 <pedrohmf51@gmail.com>
2025-10-14 18:18:31 -0300

    Base pronta

--- Trabalho-POO-Tabuleiro/src/main/java/com/faculdade/tabuleiro/Casa.java
+++ Trabalho-POO-Tabuleiro/src/main/java/com/faculdade/tabuleiro/Casa.java
@@ -3,13 +3,42 @@ import com.faculdade.controle.Jogo;
 import com.faculdade.jogador.Jogador;
 import java.util.ArrayList;
 
-public abstract class Casa {
-    protected int numero;
-    protected ArrayList<Jogador> jogadoresNaCasa;
+public class Casa {
+
+    private final int numero; //Número da casa (Ex: 1 - 40)
+    private TipoCasa tipo; //Tipo da casa (NORMAL, SORTE, MÁGICA, ETC.)
     
-    public Casa(int numero) {
-        this.numero=numero;
+    /*
+    Construtor - cria uma casa normal com um número específico
+    */
+    
+    public Casa(int numero){
+        this.numero = numero;
+        this.tipo = TipoCasa.NORMAL;
     }
     
-    public abstract void aplicarEfeito(Jogador jogador, Jogo jogo);
+    //Retorna o número da casa
+    public int getNumero(){
+        return numero;
+    }
+    
+    //Retorna o tipo da casa
+    public TipoCasa getTipo(){
+        return tipo;
+    }
+    
+    //Define o tipo da casa
+    public void setTipo(TipoCasa tipo){
+        this.tipo = tipo;
+    }
+    
+    @Override
+    public String toString(){
+         return String.format("Casa %02d (%s)", numero, tipo);
+         /*
+           %02d - tem que ter dois zeros a esquerda - 
+            Ex: ("%02d", 0) - saída: 00 
+                ("%02d", 3) - saída: 03 
+         */
+    }
 }
