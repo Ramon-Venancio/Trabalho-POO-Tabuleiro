bd0697933947de2372c1f21532c2faf7bc65f486
Pedro-hmf0906 <pedrohmf51@gmail.com>
2025-10-14 14:14:37 -0300

    Teste 1

--- Trabalho-POO-Tabuleiro/src/main/java/com/faculdade/tabuleiro/Casa.java
+++ Trabalho-POO-Tabuleiro/src/main/java/com/faculdade/tabuleiro/Casa.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.faculdade.tabuleiro;
 import com.faculdade.controle.Jogo;
 import com.faculdade.jogador.Jogador;
 import java.util.ArrayList;
 
-/**
- *
- * @author vinan
- */
+
 public abstract class Casa {
     protected int numero;
     protected ArrayList<Jogador> jogadoresNaCasa;
