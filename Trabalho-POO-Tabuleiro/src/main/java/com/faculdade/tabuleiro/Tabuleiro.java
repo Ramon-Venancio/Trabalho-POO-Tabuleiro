127447781edb0404f611f874995409b9a0c252dd
Pedro-hmf0906 <pedrohmf51@gmail.com>
2025-10-14 12:52:53 -0300

    Testando

--- Trabalho-POO-Tabuleiro/src/main/java/com/faculdade/tabuleiro/Tabuleiro.java
+++ Trabalho-POO-Tabuleiro/src/main/java/com/faculdade/tabuleiro/Tabuleiro.java
@@ -1,14 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.faculdade.tabuleiro;
 import java.util.ArrayList;
 
-/**
- *
- * @author vinan
- */
 public class Tabuleiro {
     private ArrayList<Casa> casas;
     
